266de50d11852749468a6a683d23468ebfbb5872
Andrew Telesca <justalocalhero@gmail.com>
2020-04-17 22:51:34 -0400

    Test

---
+++ Assets/_Scripts/RecordMouse.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecordMouse : MonoBehaviour
+{
+    private Camera mainCamera;
+    private float timer;
+    public float maxTimer;
+
+    public void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    public void Update()
+    {
+        if(timer >= 0)
+        {
+            timer -= Time.deltaTime;
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+        
+        }
+        
+        if(Input.GetMouseButtonUp(0))
+        {
+            timer = 0;
+        }
+        else
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LD46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
